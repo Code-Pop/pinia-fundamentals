3abdaf6f1ca02bacd3bbd2533d2769d683dcedbc
Andy <andy@Andys-MacBook-Pro.local>
2022-05-29 00:13:09 -0400

    actions

--- src/stores/useTodoListStore.js
+++ src/stores/useTodoListStore.js
@@ -4,4 +4,14 @@ export const useTodoListStore = defineStore('todoList', {
   state: () => ({
     todoList: [],
   }),
+  actions: {
+    addTodo(item) {
+      this.todoList.push({ item, id: this.id++, completed: false })
+    },
+    deleteTodo(itemId) {
+      this.todoList = this.todoList.filter((object) => {
+        return object.id !== itemId;
+      });
+    },
+  },
 })
