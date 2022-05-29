d538a458db68476656edfe3fea894765b17ee28e
Andy <andy@Andys-MacBook-Pro.local>
2022-05-28 23:58:39 -0400

    layout

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,15 @@
+<script setup>
+import TodoForm from './TodoForm.vue'
+import TodoList from './TodoList.vue'
+</script>
+
+<template>
+  <div class="todo-app">
+    <h1>To Do List</h1>
+    <todo-form />
+    <todo-list />
+  </div>
+</template>
+
+<style scoped>
+</style>
