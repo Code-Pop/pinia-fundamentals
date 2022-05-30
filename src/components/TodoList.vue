03edb7ed52c15797e88c2bdf9d06ecab063c4af3
Andy <andy@Andys-MacBook-Pro.local>
2022-05-30 18:07:21 -0400

    mark as completed

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -7,13 +7,22 @@ const store = useTodoListStore();
 // storeToRefs lets todoList keep reactivity:
 const { todoList } = storeToRefs(store)
 
+// destructuring action method doesn't require using storeToRefs:
+const { toggleCompleted } = store
+
 </script>
 
 <template>
   <div v-for="todo in todoList" :key="todo.id">
-    <div>{{ todo.item }}</div>
+    <div>
+      <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
+      <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.completed {
+  text-decoration: line-through;
+}
 </style>
