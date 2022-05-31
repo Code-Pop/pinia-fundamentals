7da3a98685d0401a6096e231308ec02c96fa0a79
Andy <andy@Andys-MacBook-Pro.local>
2022-05-31 15:15:36 -0400

    style

--- src/App.vue
+++ src/App.vue
@@ -7,4 +7,11 @@ import TodoApp from './components/TodoApp.vue'
 </template>
 
 <style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 </style>
