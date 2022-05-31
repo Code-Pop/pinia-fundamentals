1dadf0147f98cb25b0e72df624361c67fbfccd96
Andy <andy@Andys-MacBook-Pro.local>
2022-05-31 14:55:56 -0400

    actions

--- src/stores/useTodoListStore.js
+++ src/stores/useTodoListStore.js
@@ -2,6 +2,17 @@ import { defineStore } from 'pinia'
 
 export const useTodoListStore = defineStore('todoList', {
   state: () => ({
-    todoList: []
-  })
+    todoList: [],
+    id: 0
+  }),
+  actions: {
+    addTodo(item) {
+      this.todoList.push({ item, id: this.id++, completed: false })
+    },
+    deleteTodo(itemId) {
+      this.todoList = this.todoList.filter((object) => {
+        return object.id !== itemId
+      })
+    }
+  }
 })
