20b596e93e97e83c23128045f68ae16900258702
Andy <andy@Andys-MacBook-Pro.local>
2022-05-31 15:15:28 -0400

    submit todo

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -1,8 +1,26 @@
 <script setup>
+import { ref } from 'vue'
+import { useTodoListStore } from '@/stores/useTodoListStore'
+
+const todo = ref('')
+const store = useTodoListStore()
+
+function addItemAndClear(item) {
+  if (item.length === 0) {
+    return
+  }
+  // invokes function in the store:
+  store.addTodo(item)
+  todo.value = ''
+}
 </script>
 
 <template>
-  <div></div>
+  <div>
+    <form @submit.prevent="addItemAndClear(todo)">
+      <input v-model="todo" type="text" /><button>Add</button>
+    </form>
+  </div>
 </template>
 
 <style scoped>
