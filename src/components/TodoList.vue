fc6c6adebb35b19a4a7c1b7f75a3d4aab2410ac2
Andy <andy@Andys-MacBook-Pro.local>
2022-06-01 14:19:59 -0400

    class

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -13,8 +13,8 @@ const { toggleCompleted } = store
 </script>
 
 <template>
-  <div v-for="todo in todoList" :key="todo.id" class="list">
-    <div class="item">
+  <div v-for="todo in todoList" :key="todo.id" class="item">
+    <div class="content">
       <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
       <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
     </div>
